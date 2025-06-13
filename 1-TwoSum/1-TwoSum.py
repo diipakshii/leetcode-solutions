d227661fd98939c368702458992c257f2a2b7513
Dipakshi Pal <150336230+diipakshii@users.noreply.github.com>
2025-06-13 14:40:36 -0400

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.py
@@ -0,0 +1,16 @@
+# Last updated: 6/13/2025, 11:40:35 AM
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(i + 1 , len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        
+        return []
+
+        # O(n^2) time complexity
