f3be5bdb08d80268ee90c4ec6d05ca2d2c7f816b
Dipakshi Pal <150336230+diipakshii@users.noreply.github.com>
2025-06-18 22:36:11 -0700

    Create 383-RansomNote

---
+++ 383-RansomNote/383-RansomNote.py
@@ -0,0 +1,27 @@
+# Last updated: 6/18/2025, 10:36:11 PM
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        hash = {}
+
+        for char in magazine:
+            if char not in hash:
+                hash[char] = 1
+            else:
+                hash[char] += 1
+        
+        for c in ransomNote:
+            if c in hash:
+                hash[c] -= 1
+
+                if hash[c] == 0:
+                    del hash[c]
+            else:
+                return False
+        
+        return True
+        
