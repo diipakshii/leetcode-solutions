77abff32e094959a3ea1a709ec438bad76eddad5
Dipakshi Pal <150336230+diipakshii@users.noreply.github.com>
2025-07-04 21:23:03 -0700

    File updated at 7/5/2025, 12:23:03 AM

--- 66-PlusOne/66-PlusOne.py
+++ 66-PlusOne/66-PlusOne.py
@@ -1,23 +1,16 @@
-# Last updated: 7/5/2025, 12:19:37 AM
+# Last updated: 7/5/2025, 12:23:03 AM
 class Solution(object):
     def plusOne(self, digits):
         """
         :type digits: List[int]
         :rtype: List[int]
         """
-        n = len(digits) - 1
-        int_sum = 0
-
-        for digit in digits:
-            val = (10 ** n) * digit
-            int_sum += val
-            n -= 1
+        # recursion
+        if not digits:
+            return [1]
         
-        int_sum += 1
-
-        string = str(int_sum)
-
-        digit_list = [int(dig) for dig in str(int_sum)]
-
-        return digit_list
-        
+        if digits[-1] < 9:
+            digits[-1] += 1
+            return digits
+        else:
+            return self.plusOne(digits[:-1]) + [0]
