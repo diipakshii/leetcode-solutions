2b7735f97cbabe30550a843d1447c52513e0ec22
Dipakshi Pal <150336230+diipakshii@users.noreply.github.com>
2025-07-07 22:49:44 -0700

    File updated at 7/7/2025, 10:49:44 PM

--- 2918-MinimumEqualSumofTwoArraysAfterReplacingZeros/2918-MinimumEqualSumofTwoArraysAfterReplacingZeros.py
+++ 2918-MinimumEqualSumofTwoArraysAfterReplacingZeros/2918-MinimumEqualSumofTwoArraysAfterReplacingZeros.py
@@ -1,4 +1,4 @@
-# Last updated: 7/7/2025, 10:46:51 PM
+# Last updated: 7/7/2025, 10:49:44 PM
 class Solution(object):
     def minSum(self, nums1, nums2):
         """
@@ -6,26 +6,23 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: int
         """
+        # using count 
         
         sum1 = 0
         sum2 = 0
 
-        count1 = 0
-        count2 = 0
+        count1 = nums1.count(0)
+        count2 = nums2.count(0)
 
         for num in nums1:
-            sum1 += num
-            
-            if num == 0:
-                count1 += 1
-                sum1 += 1
+            sum1 += num 
+
+        sum1 += count1
         
         for num in nums2:
             sum2 += num
 
-            if num == 0:
-                count2 += 1
-                sum2 += 1
+        sum2 += count2
         
         if (count1 == 0 and sum2 > sum1) or (count2 == 0 and sum1 > sum2):
             return -1
