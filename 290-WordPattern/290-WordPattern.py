92e8b577484b83a80abaa843c74f1ba6fe0cbec4
Dipakshi Pal <150336230+diipakshii@users.noreply.github.com>
2025-07-14 23:52:06 -0400

    File updated at 7/14/2025, 8:52:06 PM

--- 290-WordPattern/290-WordPattern.py
+++ 290-WordPattern/290-WordPattern.py
@@ -1,26 +1,27 @@
-# Last updated: 7/14/2025, 8:46:29 PM
+# Last updated: 7/14/2025, 8:52:06 PM
 class Solution(object):
     def longestConsecutive(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums) == 0:
-            return 0
-
-        nums.sort()
-
-        max_len = 1
-        curr = 1
-
-        for i in range(1, len(nums)):
-            if nums[i] != nums[i - 1]:
-                if nums[i] == nums[i - 1] + 1:
-                    curr += 1
-                else: 
-                    max_len = max(max_len, curr)
-                    curr = 1
         
-        return max(max_len, curr)
+        max_len = 0
+        
+        hashmap = set(nums)
+
+        for num in hashmap:
+            if num - 1 not in hashmap:
+                curr = num
+                len = 1
+
+                while curr + 1 in hashmap:
+                    curr += 1
+                    len += 1
+                
+                max_len = max(max_len, len)
+        
+        return max_len
+
             
         
