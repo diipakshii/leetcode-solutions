b47959157909ece7d74f4b00dc1b60c0e2c3f9a1
Dipakshi Pal <150336230+diipakshii@users.noreply.github.com>
2025-07-14 23:22:56 -0400

    File updated at 7/14/2025, 8:22:56 PM

--- 290-WordPattern/290-WordPattern.py
+++ 290-WordPattern/290-WordPattern.py
@@ -1,30 +1,24 @@
-# Last updated: 7/14/2025, 7:58:24 PM
+# Last updated: 7/14/2025, 8:22:56 PM
 class Solution(object):
-    def isIsomorphic(self, s, t):
+    def containsNearbyDuplicate(self, nums, k):
         """
-        :type s: str
-        :type t: str
+        :type nums: List[int]
+        :type k: int
         :rtype: bool
         """
+        seen = set()
 
-        if len(s) != len(t):
-            return False
+        for i in range(len(nums)):
+            if nums[i] in seen:
+                return True
+            
+            seen.add(nums[i])
+            if len(seen) > k:
+                seen.remove(nums[i - k])
 
-        s_to_t = {}
-        t_to_s = {}
+        return False
+            
 
-        for i in range(len(s)):
-            s_char = s[i]
-            t_char = t[i]
-            if s_char in s_to_t:
-                if s_to_t[s_char] != t_char:
-                    return False
-            else:
-                if t_char in t_to_s:
-                    return False
-                s_to_t[s_char] = t_char
-                t_to_s[t_char] = s_char
-        
-        return True
-        
+
+            
         
