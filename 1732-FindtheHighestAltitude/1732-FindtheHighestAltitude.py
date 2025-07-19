92377bbc89a9e173b462f780dea7e27ac8d80608
Dipakshi Pal <150336230+diipakshii@users.noreply.github.com>
2025-07-19 02:39:01 -0400

    File updated at 7/18/2025, 11:39:01 PM

--- 1732-FindtheHighestAltitude/1732-FindtheHighestAltitude.py
+++ 1732-FindtheHighestAltitude/1732-FindtheHighestAltitude.py
@@ -1,17 +1,25 @@
-# Last updated: 7/18/2025, 11:24:26 PM
+# Last updated: 7/18/2025, 11:39:01 PM
 class Solution(object):
-    def largestAltitude(self, gain):
+    def findDifference(self, nums1, nums2):
         """
-        :type gain: List[int]
-        :rtype: int
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[List[int]]
         """
-        # O(n) time
 
-        curr_alt = 0
-        highest_pt = curr_alt
+        hash1 = set(nums1)
+        hash2 = set(nums2)
 
-        for val in gain:
-            curr_alt += val
-            highest_pt = max(curr_alt, highest_pt)
+        l = set()
+        m = set()
+
+        for i in nums1:
+            if i not in hash2:
+                l.add(i)
         
-        return highest_pt
+        for i in nums2:
+            if i not in hash1:
+                m.add(i)
+        
+        return [list(l), list(m)]
+        
