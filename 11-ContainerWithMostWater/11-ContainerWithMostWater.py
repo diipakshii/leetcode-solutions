267c04ccbe2888a93e71b9599f403c5835b047fb
Dipakshi Pal <150336230+diipakshii@users.noreply.github.com>
2025-08-01 21:35:56 -0700

    File updated at 8/1/2025, 9:35:56 PM

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.py
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.py
@@ -1,4 +1,4 @@
-# Last updated: 8/1/2025, 9:35:41 PM
+# Last updated: 8/1/2025, 9:35:56 PM
 class Solution(object):
     def longestOnes(self, nums, k):
         """
@@ -10,24 +10,24 @@ class Solution(object):
         left = 0
         right = 0
 
-        curr = 0
         max_count = 0
 
         while right < len(nums):
-            if nums[right] == 1:
-                curr += 1
-                right += 1
-            else: 
-                if k > 0:
-                    k -= 1
-                    curr += 1
-                    right += 1
-                else:
-                    if nums[left] == 0:
-                        k += 1
-                    curr -= 1
-                    left += 1
+            if nums[right] == 0:
+                k -= 1
             
-            max_count = max(curr, max_count)
-
+            while k < 0:
+                if nums[left] == 0:
+                    k += 1
+                
+                left += 1
+            
+            curr = right - left + 1
+            max_count = max(max_count, curr)
+            
+            right += 1
+        
         return max_count
+
+                
+        
