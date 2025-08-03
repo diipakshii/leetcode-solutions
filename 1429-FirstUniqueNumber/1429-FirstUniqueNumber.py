fcc3560cb897f3c88ff7650a498b2772b547cfab
Dipakshi Pal <150336230+diipakshii@users.noreply.github.com>
2025-08-03 00:18:23 -0700

    File updated at 8/3/2025, 12:18:23 AM

--- 1429-FirstUniqueNumber/1429-FirstUniqueNumber.py
+++ 1429-FirstUniqueNumber/1429-FirstUniqueNumber.py
@@ -1,40 +1,29 @@
-# Last updated: 8/2/2025, 4:47:00 PM
-class FirstUnique(object):
-
-    def __init__(self, nums):
+# Last updated: 8/3/2025, 12:18:23 AM
+class Solution(object):
+    def lengthOfLongestSubstringTwoDistinct(self, s):
         """
-        :type nums: List[int]
-        """
-        # initialize data structure
-        self.queue = deque()
-        self.dictionary = {}
-
-        for num in nums:
-            self.add(num)
-
-    def showFirstUnique(self):
-        """
+        :type s: str
         :rtype: int
         """
-        while self.queue and self.dictionary[self.queue[0]] > 1:
-            self.queue.popleft()
         
-        return self.queue[0] if self.queue else -1
-
-    def add(self, value):
-        """
-        :type value: int
-        :rtype: None
-        """
+        left = 0
+        right = 0
         
-        if value not in self.dictionary:
-            self.queue.append(value)
-            self.dictionary[value] = 1
-        else:
-            self.dictionary[value] += 1
+        max_len = 0
+        count = {}
 
+        for right in range(len(s)):
+            if s[right] in count:
+                count[s[right]] += 1
+            else:
+                count[s[right]] = 1
 
-# Your FirstUnique object will be instantiated and called as such:
-# obj = FirstUnique(nums)
-# param_1 = obj.showFirstUnique()
-# obj.add(value)
+            while len(count) > 2:
+                count[s[left]] -= 1
+                if count[s[left]] == 0:
+                    del count[s[left]]
+                left += 1
+
+            max_len = max(max_len, right - left + 1)
+
+        return max_len
