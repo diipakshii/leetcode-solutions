9cdeabc99dde8e39e021e723c227081fa0cb2d82
Dipakshi Pal <150336230+diipakshii@users.noreply.github.com>
2025-08-03 17:11:21 -0700

    File updated at 8/3/2025, 5:11:21 PM

--- 159-LongestSubstringwithAtMostTwoDistinctCharacters/159-LongestSubstringwithAtMostTwoDistinctCharacters.py
+++ 159-LongestSubstringwithAtMostTwoDistinctCharacters/159-LongestSubstringwithAtMostTwoDistinctCharacters.py
@@ -1,32 +1,25 @@
-# Last updated: 8/3/2025, 5:06:21 PM
+# Last updated: 8/3/2025, 5:11:21 PM
 class Solution(object):
-    def areSentencesSimilar(self, sentence1, sentence2, similarPairs):
+    def countElements(self, arr):
         """
-        :type sentence1: List[str]
-        :type sentence2: List[str]
-        :type similarPairs: List[List[str]]
-        :rtype: bool
+        :type arr: List[int]
+        :rtype: int
         """
-        if len(sentence1) != len(sentence2):
-            return False
+        # O(n) time
         
-        # add similar pairs in a hashmap
-        pairs = {}
+        elements = {}
+        count = 0
 
-        for sublist in similarPairs:
-            word1, word2 = sublist
-            if word1 in pairs:
-                pairs[word1].append(word2)
+        for num in arr:
+            if num not in elements:
+                elements[num] = 1
             else:
-                pairs[word1] = [word2]
+                elements[num] += 1
         
-        # match up similar pairs
-        for i, (word1, word2) in enumerate(zip(sentence1, sentence2)):
-            if word1 == word2:
-                continue
-            if (word1 in pairs and word2 in pairs[word1]) or (word2 in pairs and word1 in pairs[word2]):
-                continue
-            return False
-
-        return True 
+        for element in elements:
+            x = element + 1
+            if x in elements:
+                count += elements[element]
+        
+        return count
         
