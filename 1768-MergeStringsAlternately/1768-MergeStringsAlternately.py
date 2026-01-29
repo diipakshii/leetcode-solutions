ee5b5d8e4f2f5cf80bfc8e8d1befc2da64e9608f
Dipakshi Pal <150336230+diipakshii@users.noreply.github.com>
2026-01-29 00:01:39 -0500

    File updated at 1/29/2026, 12:01:39 AM

--- 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
+++ 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
@@ -1,4 +1,4 @@
-# Last updated: 1/28/2026, 11:54:26 PM
+# Last updated: 1/29/2026, 12:01:39 AM
 1class Solution(object):
 2    def mergeAlternately(self, word1, word2):
 3        """
@@ -6,17 +6,23 @@
 5        :type word2: str
 6        :rtype: str
 7        """
-8        output = ""
-9
-10        alt_len = min(len(word1), len(word2)) 
-11
-12        for i in range(alt_len):
-13            output = output + word1[i] + word2[i]
-14
-15        if len(word1) > len(word2):
-16            output = output + word1[alt_len:]
-17        else:
-18            output = output + word2[alt_len:]
+8
+9        output = ""
+10
+11        curr = 0
+12
+13        # min function has time complexity of O(n)
+14        alt_len = min(len(word1), len(word2))
+15
+16        while curr < alt_len:
+17            output = output + word1[curr] + word2[curr]
+18            curr += 1
 19
-20        return output
-21        
+20        if len(word1) > len(word2):
+21            # slicing is time complexity of O(k)
+22            output = output + word1[alt_len:]
+23        else:
+24            # slicing is time complexity of O(k)
+25            output = output + word2[alt_len:]  
+26
+27        return output
